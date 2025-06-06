8fdc6e685945ec8bc7501ff1d763e4d8451ac4e4
onsissond <onsissond@gmail.com>
2025-06-06 21:32:32 +0200

    Add example

--- app/src/main/java/com/example/pizzadelivery/GeneratedAnalyticsEvents.kt
+++ app/src/main/java/com/example/pizzadelivery/GeneratedAnalyticsEvents.kt
@@ -3,12 +3,60 @@
 package com.example.pizzadelivery
 
 internal object GeneratedAnalyticsEvents {
+  /**
+   * A screen with user data
+   */
+  internal object ProfileScreen {
 
-  internal fun mainScreenOpened(): AnalyticsEvent {
+    internal fun profileScreenShown(): AnalyticsEvent {
+      return AnalyticsEvent(
+        name = "Profile Screen Shown",
+        parameters = emptyMap()
+      )
+    }
+
+    /**
+     * Screen is fully closed
+     */
+    internal fun profileScreenClosed(): AnalyticsEvent {
+      return AnalyticsEvent(
+        name = "Profile Screen Closed",
+        parameters = emptyMap()
+      )
+    }
+  }
+
+  internal fun onboardingScreenShown(  
+    origin: Origin?
+  ): AnalyticsEvent {
     return AnalyticsEvent(
-      name = "Main Screen Opened",
-      parameters = emptyMap()
+      name = "Onboarding Screen Shown",
+      parameters = mapOf(
+        "origin" to origin
+      ).withoutNulls()
+    )
+  }
+  /**
+   * The user sees the full screen loading indicator
+   * @param city_id The id of the city
+   */
+  internal fun loadingScreenShown(  
+    cityId: String?
+  ): AnalyticsEvent {
+    return AnalyticsEvent(
+      name = "Loading Screen Shown",
+      parameters = mapOf(
+        "city_id" to cityId
+      ).withoutNulls()
     )
   }
 }
 
+// Custom types for 
+internal enum class Origin(val value: String) {
+  Facebook("Facebook"),
+  Insta("Insta")
+}
+
+private fun Map<String, Any?>.withoutNulls(): Map<String, Any> =
+    this.filterValues { it != null }.mapValues { it.value!! }
